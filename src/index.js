87a1935b0310fdaebc3fb74ca27e45a44813b33f
Shreyansh2109 <142212540+Shreyansh2109@users.noreply.github.com>
2024-03-22 14:45:24 +0530

    add new navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Temp from './project1/Temp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Temp/>
   </React.StrictMode>
 );
 
